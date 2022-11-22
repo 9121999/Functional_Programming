ddf9f5014902b50e12cfa721656be118cabc889b
Prerana <dahiwadeprerana@gmail.com>
2022-11-22 19:03:37 +0530

    [prerana]Add:FlipCoin

---
+++ Functional_Programming/FunctionProgramFlip.cs
@@ -0,0 +1,40 @@
+﻿namespace FunctionalProgram
+{
+    internal class FunctinalProgramFlip
+    {
+        Random random = new Random();
+        public double CoinToss()
+        {
+            double number = random.NextDouble();
+            return number;
+        }
+
+        public void FlipCoin()
+        {
+            double count = 0, headCount = 0, tailCount = 0, coinFlip;
+            Console.Write("Enter number of COIN flip for the toss : ");
+            coinFlip = Convert.ToInt32(Console.ReadLine());
+            while (coinFlip != count)
+            {
+                count++;
+                double toss = CoinToss();
+                if (toss < 0.5)
+                {
+                    tailCount++;
+                }
+                else
+                {
+                    headCount++;
+                }
+            }
+
+            double he = (double)((headCount * 100) / coinFlip);
+            double ta = (double)((tailCount * 100) / coinFlip);
+            Console.WriteLine("Winning percentage of HEAD is : " + he);
+            Console.WriteLine("Winning percentage of TAIL is : " + ta);
+
+        }
+    }
+}
+
+
---
+++ Functional_Programming/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FunctionalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctinalProgramFlip functinal = new FunctinalProgramFlip();
+            functinal.FlipCoin();
+        }
+    }
+}

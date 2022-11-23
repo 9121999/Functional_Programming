878231effdfbfa9d00aef5be57e0e1d3f4917091
Prerana <dahiwadeprerana@gmail.com>
2022-11-23 16:04:13 +0530

    [prerana]Add:QuoAndRemainder

--- Functional_Programming/FunctionProgramFlip.cs
+++ Functional_Programming/FunctionProgramFlip.cs
@@ -201,11 +201,36 @@ namespace FunctionalProgram
 
                     }
                 }
+                internal class Quotient
+                {
+                    public int EnterDivisor()
+                    {
+                        Console.Write("Enter the  Dividend value = ");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        return N;
+                    }
+                    public int EnterDividend()
+                    {
+                        Console.Write("Enter the Divisor value  = ");
+                        int M = Convert.ToInt32(Console.ReadLine());
+                        return M;
+                    }
+                    public void FindQuoRem()
+                    {
+                        int dividend = EnterDivisor();
+                        int divisor = EnterDividend();
+                        int quoitent = dividend / divisor;
+                        int remainder = dividend % divisor;
+                        Console.WriteLine("Quoitent is : " + quoitent);
+                        Console.WriteLine("Remainder is : " + remainder);
+
+                    }
+                }
             }
         }
-
     }
-}
+ }
+
     
 
 
--- Functional_Programming/Program.cs
+++ Functional_Programming/Program.cs
@@ -11,7 +11,7 @@ namespace FunctionalProgram
         {
             while (true)
             {
-                Console.WriteLine("\nselect program\n 1.FlipCoin\n 2.EvenOdd \n 3.Factors \n 4.FindLargest \n 5.LeapYear \n 6.PowerOfTwo \n 7.SwapTwoNumber \n 8.HarmonicNumber \n 9.VowelAndConsonant");
+                Console.WriteLine("\nselect program\n 1.FlipCoin\n 2.EvenOdd \n 3.Factors \n 4.FindLargest \n 5.LeapYear \n 6.PowerOfTwo \n 7.SwapTwoNumber \n 8.HarmonicNumber \n 9.VowelAndConsonant\n 10.QuotientAndRemainder");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -51,6 +51,10 @@ namespace FunctionalProgram
                     case 9:
                       Letters letters = new Letters();
                         letters.VowelConsonant();
+                        break;
+                    case 10:
+                        Quotient quotient = new Quotient();
+                        quotient.FindQuoRem();
                         break;
                 }
             }
